be26d8d98beb49d0ed547a3ef80850d953d635d1
winniewyl <90722768+winniewyl@users.noreply.github.com>
2022-01-12 16:18:38 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import sketch from './sketch';
 import p5 from 'p5';
 
-const TEXT = 'Happy Birthday Trisha';
+//nested components
+import Header from './components/Header';
+import Cake from './components/Cake';
+import Gifts from './components/Gifts';
+
+const TEXT = 'Happy Birthday Dad';
 
 const App = () => {
-  const canvasRef = React.useRef(null)
+  const canvasRef = React.useRef(null);
 
   React.useEffect(() => {
     const canvasDivElement = canvasRef.current;
@@ -14,13 +19,79 @@ const App = () => {
     // NOTE: If you change TEXT value to something else, you'll need to make changes in sketch.js as well to make it work
     // See my comments in sketch.js
     new p5(sketch(canvasDivElement, TEXT), canvasDivElement);
-  })
+  });
 
   return (
-    <div className="main">
-      <div ref={canvasRef} />
-    </div >
-  )
-}
+    <>
+      <div className="main">
+        <div ref={canvasRef} />
+      </div>
+      <Cake />
+      <Header />
+      <Gifts />
+    </>
+  );
+};
 
 export default App;
+
+// //import logo from './logo.svg';
+// import './App.css';
+// //imr
+// import React from 'react';
+// //imrd
+
+// import ReactDOM from 'react-dom';
+// import Header from './components/Header';
+// import Clock from './components/Clock';
+
+// import Cake from './components/Cake';
+// import Animation from './components/Animation';
+// import Gifts from './components/Gifts';
+
+// const App = () => {
+//   return <div className="container"></div>;
+// };
+
+// ReactDOM.render(
+//   <>
+//     <React.StrictMode>
+//       <Animation />
+//     </React.StrictMode>
+
+//     {/* <Header /> */}
+//     <Clock />
+
+//     <Cake />
+//     <Gifts />
+//   </>,
+//   document.getElementById('root')
+// );
+
+// export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
